54b22e11936d3ee8fd2299783e659b399d1ee9c7
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-02-16 18:09:58 -0500

    initial setup

---
+++ src/core.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class Adventurer:
+    
---
+++ src/jobs.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Job(Enum):
+    WARRIOR = {
+        "name": "warrior"
+    }
+
+    BLACK_MAGE = {
+        "name": "black mage"
+    }
