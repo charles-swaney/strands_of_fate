202d398120513dbd6f1397cd20097bd48e8a7ccc
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-02-16 20:50:20 -0500

    set up adventurer attributes

--- src/core.py
+++ src/core.py
@@ -1,7 +1,35 @@
 from dataclasses import dataclass
-from enum import Enum
-
+from typing import Dict
+from src.jobs.base import Job
 
 @dataclass
 class Adventurer:
-    
+    name: str
+    job: Job
+    level: int = 1
+    levels_gained: Dict[str, int]
+
+    # Combat Stats
+    hp: float
+    mp: float
+    strength: float
+    toughness: float
+    dexterity: float
+    agility: float
+    intellect: float
+    willpower: float
+
+    watk: float
+    wdef: float
+    matk: float
+    mdef: float
+
+    # Auxiliary Stats
+    tenacity: float
+    charisma: float
+    aptitude: float
+    luck: float
+
+    def level_up(self):
+        self.level += 1
+        self.job.on_level_up(self)
