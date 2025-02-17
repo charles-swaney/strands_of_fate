3d08c1b1be2726063d2196178419d05240dac866
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-02-17 18:59:20 -0500

    implement job_name, class_aptitude abstract methods

--- src/jobs/job.py
+++ src/jobs/job.py
@@ -12,6 +12,16 @@ class Job(ABC):
     def growth_rates(self) -> Dict[str, int]:
         pass
 
+    @property
+    @abstractmethod
+    def job_name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def class_aptitude(self) -> int:
+        pass
+
     @abstractmethod
     def apply_level_up(self, adventurer: Adventurer) -> None:
         pass
