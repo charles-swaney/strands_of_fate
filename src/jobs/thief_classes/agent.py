a9e709834155a7a40c177154901df8fa85fce6c6
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-02-17 21:17:13 -0500

    add new class

---
+++ src/jobs/thief_classes/agent.py
@@ -0,0 +1,66 @@
+from src.jobs.job import Job
+from typing import Dict, TYPE_CHECKING
+from src.adventurers.stat_growth import compute_stat_bonus
+
+if TYPE_CHECKING:
+    from src.jobs.job import Adventurer
+
+
+class Agent(Job):
+    """
+    An incredibly fast class, focusing on dealing consistent but low damage,
+    and buffing allies' offense.
+
+    Key Traits:
+    - Amazing speed, dexterity, agility, charisma growth.
+    - Very strong stat growths in general.
+    - Abilities enhance ally damage and apply status effects.
+    - Flexibility to equip ranged weapons.
+
+    Weapons:
+    - Swords, Daggers, Bows
+
+    Armor:
+    - Light armor, Robes
+    """
+    @property
+    def growth_rates(self) -> Dict[str, int]:
+        # Total: 54
+        return {
+            "hp": 5,
+            "mp": 5,
+            "strength": 4,
+            "toughness": 3,
+            "dexterity": 8,
+            "agility": 8,
+            "intellect": 3,
+            "wisdom": 3,
+            "speed": 10,
+            "tenacity": 3,
+            "charisma": 7,
+            "luck": 5
+        }
+    
+    @property
+    def class_aptitude(self) -> int:
+        return -0.5
+    
+    @property
+    def job_name(self) -> str:
+        return "Agent"
+
+    def apply_level_up(self, adventurer: "Adventurer") -> None:
+        base_growth_rates = self.growth_rates
+
+        for stat, growth_rate in base_growth_rates.items():
+            if growth_rate <= 3:
+                bonus_mult = 0.5
+            elif growth_rate >= 4 and growth_rate <= 8:
+                bonus_mult = 1
+            else:
+                bonus_mult = 1.5
+            stat_bonus = compute_stat_bonus(
+                base_aptitude=adventurer.aptitude,
+                class_aptitude=self.class_aptitude)
+            
+            adventurer.base_stats[stat] += growth_rate + (bonus_mult * stat_bonus)
