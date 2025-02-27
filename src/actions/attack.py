7915d8fe1e6428b1e31207189c140526d393b679
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-02-27 16:32:37 -0500

    implement basic physical attack

---
+++ src/actions/attack.py
@@ -0,0 +1,69 @@
+from typing import Union, List
+import random
+
+from actions.action import Action
+from monsters.monster import Monster
+from adventurers.adventurer import Adventurer
+from combat.damage_calculator import compute_damage_physical
+from combat.hit_chance import compute_hit_chance
+from combat.crit_chance import compute_critical_chance
+
+CRIT_DAMAGE_MULT = 1.50
+BLOCKING_EFFECTS = {
+    "Stun",
+    "Petrify",
+    "Stop"
+}
+
+
+class Attack(Action):
+    """A class representing a basic physical attack."""
+    def __init__(self,
+                 name: str="Attack",
+                 cost: int=0,
+                 target_type: str="single",
+                 cost_type: str="mp",
+                 cooldown: int=0):
+        super().__init__(name, cost, target_type, cost_type, cooldown)
+        self.name = name
+        self.cost = cost
+        self.target_type = target_type
+        self.cost_type = cost_type
+        self.remaining_cooldown = 0
+    
+    def execute(self,
+                attacker: Union[Adventurer, Monster],
+                targets: List[Union[Adventurer, Monster]]):
+        if not self.can_be_used(attacker):
+            raise ValueError(f"Cannot attack right now.")
+        
+        for target in targets:
+            hit_chance = compute_hit_chance(attacker, target)
+            hit_roll = random.random()
+
+            if hit_roll < hit_chance:
+                crit_chance = compute_critical_chance(attacker, target)
+                crit_roll = random.random()
+                crit_dmg_mult = CRIT_DAMAGE_MULT if crit_roll < crit_chance else 1.0
+
+                damage = compute_damage_physical(attacker,
+                                                target,
+                                                "standard",
+                                                attacker.weapon_type,
+                                                crit_dmg_mult)
+                target.update_hp(-damage)
+            else:
+                pass
+
+    def can_be_used(self, attacker: Union[Adventurer, Monster]) -> bool:
+        """
+        A unit can always attack, unless they have a blocking status effect.
+        """
+        for status in attacker.status_effects:
+            if status.name in BLOCKING_EFFECTS:
+                return False
+        return True
+    
+    def tick_cooldown(self) -> None:
+        if self.remaining_cooldown > 0:
+            self.remaining_cooldown -= 1
