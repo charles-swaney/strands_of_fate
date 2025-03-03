8c2ee626c4b0f0b0df454aad8a94d5e796e383a0
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-02 21:14:28 -0500

    implement thunderbolt

---
+++ src/combat/skills/black_mage/thunderbolt.py
@@ -0,0 +1,16 @@
+from actions.spell import Spell
+
+
+class Thunderbolt(Spell):
+    def __init__(self):
+        super().__init__(
+            name="Thunderbolt",
+            cost_type="mp",
+            base_cost=12,
+            cost_scaling=1.25,
+            cooldown = 3,
+            magnitude=1.25,
+            element = "lightning",
+            spell_type="damage"
+        )
+        self.target_type = "multiple"
