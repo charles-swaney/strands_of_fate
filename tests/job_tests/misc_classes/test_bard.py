1c7cd3cc1521ee8703480dbd6943a173140bcf8d
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-15 19:27:24 -0400

    add tests for fortifying chant, spunky ditty

--- tests/job_tests/misc_classes/test_bard.py
+++ tests/job_tests/misc_classes/test_bard.py
@@ -1,11 +1,13 @@
 import pytest
 from pytest import approx
-from unittest.mock import patch
+from operator import add
 from adventurers.adventurer import Adventurer
 from jobs.misc_classes.bard import Bard
 from jobs.warrior_classes.fighter import Fighter
-from combat.skills.bard.torching_tempo import TorchingTempo
+from jobs.mage_classes.spellblade import SpellBlade
+from combat.skills.bard import TorchingTempo, FortifyingChant, SpunkyDitty
 from core.stats.attributes import Attributes
+START_MP = 56
 
 
 @pytest.fixture
@@ -30,30 +32,98 @@ def fighter_():
     )
     return a
 
-def test_tt(bard_, fighter_):
+@pytest.fixture
+def sb_():
+    c = SpellBlade()
+    a = Adventurer(
+        name="",
+        job=c,
+        level=5,
+        deterministic=True
+    )
+    return a
+
+
+def test_tt(bard_, fighter_, sb_):
     tt = TorchingTempo()
     b = bard_
     f = fighter_
+    sb = sb_
     assert b.hp == 40
-    START_MP = 56
 
     START_STR, START_DEX = 45, 25
-    STR_INCREASE_1 = 21.31
-    DEX_INCREASE_1 = 19.95
+    STR_INCREASE_1 = 24.7025
+    DEX_INCREASE_1 = 23.6125
     b.use(tt, f)
     tt.remaining_cooldown = 0
-    assert f.total_stats.get_stat("strength") == START_STR + STR_INCREASE_1
-    assert f.total_stats.get_stat("dexterity") == START_DEX + DEX_INCREASE_1
+    assert f.strength == START_STR + STR_INCREASE_1
+    assert f.dexterity == START_DEX + DEX_INCREASE_1
     f.stat_buffs = Attributes({})
-    assert f.total_stats.get_stat("strength") == START_STR
+    assert f.strength == START_STR
+    assert b.mp == approx(START_MP - 9.93979)
     for _ in range(94):
         b.level_up()
         f.level_up()
     NEW_STR, NEW_DEX = 891, 495
-    assert f.total_stats.get_stat("strength") == NEW_STR
-    assert f.total_stats.get_stat("dexterity") == NEW_DEX
-    STR_INCREASE_2 = 629.4524
-    DEX_INCREASE_2 = 468.518
+    assert f.strength == NEW_STR
+    assert f.dexterity == NEW_DEX
+    STR_INCREASE_2 = 470.4881
+    DEX_INCREASE_2 = 415.4045
     b.use(tt, f)
-    assert f.total_stats.get_stat("strength") == NEW_STR + STR_INCREASE_2
-    assert f.total_stats.get_stat("dexterity") == NEW_DEX + DEX_INCREASE_2
+    assert f.strength == NEW_STR + STR_INCREASE_2
+    assert f.dexterity == NEW_DEX + DEX_INCREASE_2
+    with pytest.raises(Exception) as e:
+        b.use(tt, [f, sb])
+
+def test_fc(bard_, fighter_):
+    fc = FortifyingChant()
+    b = bard_
+    f = fighter_
+    START_TGH= 25
+    START_TEN = 25
+    TGH_INCREASE_1 = 23.6125
+    TEN_INCREASE_1 = 23.6125
+    b.use(fc, f)
+    fc.remaining_cooldown = 0
+    assert f.toughness == START_TGH + TGH_INCREASE_1
+    assert f.tenacity == START_TEN + TEN_INCREASE_1
+    assert b.mp == approx(START_MP - 10.333770877266215)
+
+
+def test_sd(bard_, fighter_, sb_):
+    sd = SpunkyDitty()
+    LVL_5_COST = 19.8795834132113
+    LVL_99_COST = 59.26094175893539
+    b = bard_
+    INIT_MP = b.mp
+    f = fighter_
+    sb = sb_
+    fighter_lvl_5_stats = [9 * 5, 2 * 5, 4 * 5, 6 * 5]
+    sb_lvl_5_stats = [6 * 5, 6 * 5, 6 * 5, 6 * 5]
+    assert [f.strength, f.intellect, f.agility, f.luck] == fighter_lvl_5_stats
+    assert [sb.strength, sb.intellect, sb.agility, sb.luck] == sb_lvl_5_stats
+    b.use(sd, [f, sb])
+    sd.remaining_cooldown = 0
+    assert b.mp == approx(INIT_MP - LVL_5_COST)
+    assert [f.strength, f.intellect, f.agility, f.luck] == \
+        approx(list(map(add, fighter_lvl_5_stats, [24.7025, 22.795, 23.34, 23.885])))
+    assert [sb.strength, sb.intellect, sb.agility, sb.luck] == \
+        approx(list(map(add, sb_lvl_5_stats, [23.885, 23.885, 23.885, 23.885])))
+    
+    for _ in range(94):
+        b.level_up()
+        f.level_up()
+        sb.level_up()
+    f_99_stats = [9 * 99, 2 * 99, 4 * 99, 6 * 99]
+    sb_99_stats = [6 * 99, 6 * 99, 6 * 99, 6 * 99]
+    f.stat_buffs = Attributes({})
+    sb.stat_buffs = Attributes({})
+    assert [f.strength, f.intellect, f.agility, f.luck] == f_99_stats
+    assert [sb.strength, sb.intellect, sb.agility, sb.luck] == sb_99_stats
+    b.mp += 1000
+    b.use(sd, [f, sb])
+    assert b.mp == approx(714 - LVL_99_COST)
+    assert [f.strength, f.intellect, f.agility, f.luck] == \
+        approx(list(map(add, f_99_stats, [470.4881, 374.0918, 401.6336, 429.1754])))
+    assert [sb.strength, sb.intellect, sb.agility, sb.luck] == \
+        approx(list(map(add, sb_99_stats, [429.1754, 429.1754, 429.1754, 429.1754])))
