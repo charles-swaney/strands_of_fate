0e2f4379f042c66864f863ffd1f755ba9df2e7f2
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-15 19:25:49 -0400

    add ice_ii

--- src/combat/skills/black_mage/ice_ii.py
+++ src/combat/skills/black_mage/ice_ii.py
@@ -1,16 +1,19 @@
 from actions.skill import Skill
 
 
-class Fire_II(Skill):
+class Ice_II(Skill):
     def __init__(self):
+        """
+        Describe the Ice_II skill.
+        """
         super().__init__(
-            name="Fire",
+            name="Ice",
             cost_type="mp",
             base_cost=6,
             cost_scaling=1.5,
             cooldown=3,
             magnitude=1.10,
-            element="fire",
+            element="Ice",
             skill_type="damage"
         )
         self.target_type="multiple"
