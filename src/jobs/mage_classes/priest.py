85284d3380f5cfece7e65b61cf31a0f1ee93926f
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-15 20:00:11 -0400

    update to allow for deterministic growth, add growth rates in description

--- src/jobs/mage_classes/priest.py
+++ src/jobs/mage_classes/priest.py
@@ -22,6 +22,20 @@ class Priest(Job):
 
     Armor:
     - Robes
+
+    Growths:
+        "hp": 5,
+        "mp": 7,
+        "strength": 3,
+        "toughness": 5,
+        "dexterity": 5,
+        "agility": 4,
+        "intellect": 5,
+        "wisdom": 9,
+        "speed": 5,
+        "tenacity": 7,
+        "charisma": 6,
+        "luck": 7
     """
 
     @property
@@ -78,12 +92,15 @@ class Priest(Job):
         growth_rates = self.growth_rates
 
         for stat, growth_rate in growth_rates.items():
-            if growth_rate <= 3:
-                bonus_mult = 0.5
-            elif growth_rate >= 4 and growth_rate <= 8:
-                bonus_mult = 1
+            if adventurer.deterministic:
+                bonus_mult = 0
             else:
-                bonus_mult = 1.5
+                if growth_rate <= 3:
+                    bonus_mult = 0.5
+                elif growth_rate >= 4 and growth_rate <= 8:
+                    bonus_mult = 1
+                else:
+                    bonus_mult = 1.5
             stat_bonus = compute_stat_bonus(
                 base_aptitude=adventurer.aptitude,
                 class_aptitude=self.class_aptitude
