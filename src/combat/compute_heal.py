4c22c8b768b757d20eb5d55c751f5c6cb60c3b35
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-16 20:03:29 -0400

    add compute raw heal, for heals that scale with other stats than wisdom

--- src/combat/compute_heal.py
+++ src/combat/compute_heal.py
@@ -37,3 +37,28 @@ def compute_heal(caster: Union[Adventurer, Monster],
     final_heal *= random.uniform(0.95, 1.05)
 
     return max(MIN_HEAL, min(MAX_HEAL, final_heal))
+
+
+def compute_heal_raw(heal_value: float,
+                     multipliers: List[float] = []) -> float:
+    """
+    Computes the heal value using raw values. Used only if the caster is using a skill whose
+    heal value is not computed using wisdom, charisma, luck.
+
+    Args:
+        heal_value: the quantity to be used in place of, essentially, wisdom.
+        multipliers: buffs, etc.
+
+    Returns:
+        float: the heal amount.
+    """
+    if multipliers is None:
+        multipliers = []
+    
+    main_heal = 1.05 * (0.43 * heal_value)
+
+    final_heal = main_heal * prod(multipliers) if multipliers else main_heal
+
+    final_heal *= random.uniform(0.95, 1.05)
+
+    return max(MIN_HEAL, min(MAX_HEAL, final_heal))
