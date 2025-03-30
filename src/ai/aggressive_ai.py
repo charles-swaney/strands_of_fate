66957d904d4174925402c6c86d835e97d2b67912
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-30 18:27:28 -0400

    implement smart aggressive AI skill ordering

--- src/ai/aggressive_ai.py
+++ src/ai/aggressive_ai.py
@@ -5,10 +5,28 @@ from battles.battle import Battle
 from adventurers.adventurer import Adventurer
 from monsters.monster import Monster
 from actions.action import Action
-from ai.ai import AIBehavior
+from ai.ai_behavior import AIBehavior
+from ai.behavior_nodes.behavior_node import Selector
+from ai.behavior_nodes.damage_skill_nodes import UseMultiTargetDamageSkill, UseDamageSkill, BasicAttackPreferLowHPTarget, BasicAttackRandomTarget
+from ai.behavior_nodes.debuff_skill_nodes import UseMultiTargetDebuffSkill, UseDebuffSkill
+from ai.behavior_nodes.buff_skill_nodes import UseMultiTargetBuffSkill, UseBuffSkill, UseSelfBuffSkill
 
 
-class AggressiveAI(AIBehavior):
+class SmartAggressiveAI(AIBehavior):
+    def __init__(self, owner):
+        super().__init__(owner)
+        
+        self.behavior_tree = Selector([
+            UseMultiTargetDamageSkill(),
+            UseSelfBuffSkill(),
+            UseMultiTargetDebuffSkill(),
+            UseDamageSkill(),
+            BasicAttackPreferLowHPTarget(),
+            UseDebuffSkill(),
+            UseBuffSkill(),
+            BasicAttackRandomTarget()
+        ])
+
     def do_action(self, battle: Battle):
         """
         Target selection for aggressive units. Aggressive units prioritize dealing damage to as
@@ -23,51 +41,8 @@ class AggressiveAI(AIBehavior):
         """
         unit = self.owner
 
-        opposing_side = battle.adventurers if isinstance(unit, Monster) else battle.monsters
+        data = self.setup_data(battle=battle)
 
-        all_targets = [unit for unit in opposing_side if unit.hp > 0]
-        if not all_targets:
-            return
+        self.behavior_tree.execute(unit, battle, data) 
 
-        unit_atk_skills = [skill for skill in unit._all_known_skills if skill.skill_type == "damage"]
-        unit_debuff_skills = [skill for skill in unit._all_known_skills if skill.skill_type == "debuff"]
-
-        available_atk_skills = [skill for skill in unit_atk_skills if skill.can_be_used(unit)]
-        available_debuff_skills = [skill for skill in unit_debuff_skills if skill.can_be_used(unit)]
-
-        available_multi_atk_skills = [skill for skill in available_atk_skills if skill.target_type != "single"]
-        available_multi_debuff_skills = [skill for skill in available_debuff_skills if skill.target_type != "single"]
-
-        if len(all_targets) > 1:
-            if available_multi_atk_skills:
-                action_choice = random.choice(available_multi_atk_skills)
-            elif available_debuff_skills:
-                action_choice = random.choice(available_multi_debuff_skills)
-            else:  # No multi attack or debuff skill available
-                action_choice = None
-            if action_choice:
-                targets = all_targets if action_choice.target_type == "all" else random.sample(all_targets, min(2, len(all_targets)))
-                unit.use(action_choice, targets)
-                return action_choice.name, targets
-        
-        target = random.choice(all_targets)
-        
-        if target.hp / target.max_hp > 0.50:
-            if available_atk_skills:
-                action_choice = random.choice(available_atk_skills)
-            elif available_debuff_skills:
-                action_choice = random.choice(available_debuff_skills)
-            else:
-                unit.attack(target)
-                print(f"Target is healthy, but no skills available. Attacked {target}")
-                return "Attack", target
-        else:  # Target has less than half hp
-            if available_atk_skills:
-                action_choice = random.choice(available_atk_skills)
-            else:
-                unit.attack(target)
-                print(f"Target is weak, no attack skills. Attacked {target}.")
-                return "Attack", target
-        print(f"Using {action_choice} on {target}.")
-        unit.use(action_choice, target)
-        return action_choice.name, target
+        return data['result']
