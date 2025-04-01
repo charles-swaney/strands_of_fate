7a12660de2fcf972e6dcb257389db1e14400c1b5
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-03-31 21:05:00 -0400

    add some combat tests for wolf ai

--- tests/ai_tests/test_aggressive.py
+++ tests/ai_tests/test_aggressive.py
@@ -5,8 +5,10 @@ from monsters.monster import Monster
 from jobs.mage_classes.black_mage import BlackMage
 from jobs.warrior_classes.fighter import Fighter
 from monsters.beasts.wolf import Wolf
+from monsters.beasts.direwolf import DireWolf
 from monsters.beasts.behemoth import Behemoth
 from monsters.beasts.behemoth_skills import Trample, ExposeWeakness, IronHide, EarthenGrasp
+from monsters.beasts.shared.aimed_strike import AimedStrike
 from unittest.mock import patch
 
 
@@ -26,6 +28,14 @@ def behemoth_():
 @pytest.fixture
 def wolf_():
     w = Wolf(
+        level=25,
+        deterministic=True
+    )
+    return w
+
+@pytest.fixture
+def dwolf_():
+    w = DireWolf(
         level=25,
         deterministic=True
     )
@@ -71,3 +81,24 @@ def test_multi_available(behemoth_, wolf_, fighter_, blackmage_):
     battle.tick_cooldowns()
     assert behemoth.do_action(battle)[0] == "Attack"
     battle.tick_cooldowns()
+
+
+def test_multi_available(behemoth_, wolf_, dwolf_):
+    behemoth, wolf, direwolf = behemoth_, wolf_, dwolf_
+    advs, monsters = [wolf, direwolf], [behemoth]
+    trample = Trample()
+    grasp = EarthenGrasp()
+    ironhide = IronHide()
+    aimed_strike = AimedStrike()
+    battle = Battle(advs, monsters)
+    behemoth.learn_skill(ironhide)
+    behemoth.learn_skill(trample)
+    behemoth.learn_skill(grasp)
+    wolf.learn_skill(aimed_strike)
+    direwolf.learn_skill(aimed_strike)
+    # R1
+    print(direwolf.do_action(battle))
+    # R2
+    print(direwolf.do_action(battle))
+    # R3
+    print(direwolf.do_action(battle))
