d90f4ab973072010d4a29c5356ed5b79da7b462e
charles-swaney <67131291+charles-swaney@users.noreply.github.com>
2025-04-03 20:36:49 -0400

    implement method to run battle until one party loses

--- src/battles/battle.py
+++ src/battles/battle.py
@@ -7,11 +7,21 @@ from battles.turn_order import get_turn_order
 class Battle:
     def __init__(self,
                  adventurers: List[Adventurer],
-                 monsters: List[Monster]):
+                 monsters: List[Monster],
+                 verbose: bool = False):
+        """
+        The general Battle class for handling combat.
+
+        Args:
+            adventurers (List[Adventurer]): the adventurers participating in the battle.
+            monsters (List[Monster]): the monsters participating in the battle.
+            verbose (bool): whether to print statements or not (for 'viewing' battles in CLI)
+        """
         self.adventurers = adventurers
         self.monsters = monsters
         self.round_count = 0
         self.turn_order = get_turn_order(self.adventurers, self.monsters)
+        self.verbose = verbose
 
     def tick_cooldowns(self) -> None:
         """
@@ -38,8 +48,14 @@ class Battle:
         if len(self.turn_order) == 0:
             self.turn_order = get_turn_order(self.adventurers, self.monsters)
             self.round_count += 1
+            if self.verbose:
+                print(f"Starting round {self.round_count}")
         
         current_unit = self.turn_order.pop(0)
+        if self.verbose:
+            print(f"Current Unit: {current_unit.name}")
+
+        print(f"Action: {current_unit.do_action(self)}")
         return current_unit
 
     def is_battle_over(self) -> bool:
@@ -51,6 +67,32 @@ class Battle:
         elif all(adv.hp == 0 for adv in self.adventurers):
             return True
         return False
+    
+    def run_battle(self) -> str:
+        """
+        Run the battle until one side is defeated.
+
+        Returns:
+            str: whichever side won the battle.
+        """
+        if self.verbose:
+            print("Battle beginning.")
+        
+        while not self.is_battle_over():
+            self.next_turn()
+            self.tick_cooldowns()
+        if all(monster.hp == 0 for monster in self.monsters):
+            outcome = "Adventurers win"
+        else:
+            outcome = "Monsters win"
+    
+        if self.verbose:
+            print(f"Battle over after {self.round_count} rounds. {outcome}")
+            adv_hps = [(adv.name, adv.hp) for adv in self.adventurers]
+            monster_hps = [(m.name, m.hp) for m in self.monsters]
+            print(f"Outcome: Adventurers: {adv_hps}. Monsters: {monster_hps}.")
+        
+        return outcome
     
     def remove_dead(self) -> None:
         """
